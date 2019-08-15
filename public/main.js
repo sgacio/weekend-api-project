a5aac45a1fd90949165132ad202432a802d040b1
Stephen Gacio <sgacio@mail.usf.edu>
2019-08-15 14:53:49 -0400

    css hmtl and fontawsome in place, api1 done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  findNasaPhoto()
+}
+
+const findNasaPhoto = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(response => {
+      return response.json()
+    }).then(nasaPhoto => {
+      console.log(nasaPhoto.hdUrl)
+      document.querySelector('.picture-background').style.backgroundImage = `url('${nasaPhoto.hdUrl}')`
+      document.querySelector('.copy-right').textContent = `copyright: ${nasaPhoto.copyright} | title: ${nasaPhoto.title}`
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
