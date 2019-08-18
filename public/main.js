ed6a992777eb95465db9cf03b9ba57d0aeb92f4e
Stephen Gacio <sgacio@mail.usf.edu>
2019-08-17 20:22:15 -0400

    most things complete

--- public/main.js
+++ public/main.js
@@ -1,14 +1,3 @@
-class Space_X_Launches {
-  constructor(name, description, time, location) {
-    this.name = name
-    this.description = description
-    this.time = time
-    this.location = location
-  }
-}
-
-let spaceXFlights = []
-
 const main = () => {
   findNasaPhoto()
   getSpaceXMissions()
@@ -19,29 +8,106 @@ const findNasaPhoto = () => {
     .then(response => {
       return response.json()
     }).then(nasaPhoto => {
-      document.querySelector('.picture-background').style.backgroundImage = `url('${nasaPhoto.hdUrl}')`
-      document.querySelector('.copy-right').textContent = `copyright: ${nasaPhoto.copyright} | title: ${nasaPhoto.title}`
+      displayPhoto(nasaPhoto)
     })
 }
 
-const getSpaceXMissions = () => {
-  fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
-    .then(response => {
-      return response.json()
-    }).then(missionData => {
-      console.log(missionData)
-      spaceXFlights = missionData.map(missionData => new Space_X_Launches(missionData.mission_name, missionData.details, missionData.launch_date_unix, missionData.launch_site.site_name_long))
-      // document.getElementById('name-of-mission').textContent = spaceXFlights[0].name
-      // document.getElementById('description-of-mission').textContent = spaceXFlights[0].description
-      // document.getElementById('countdown-clock').textContent = spaceXFlights[0].time
-      // document.getElementById('location-of-launch').textContent = spaceXFlights[0].location
-      makeSpaceXCards(spaceXFlights)
-    })
+const displayPhoto = (nasaPhoto) => {
+  document.querySelector('.picture-background').style.backgroundImage = `url('${nasaPhoto.hdUrl}')`
+  document.querySelector('.copy-right').textContent = `copyright: ${nasaPhoto.copyright} | title: ${nasaPhoto.title}`
 }
-const makeSpaceXCards = spaceXFlights => {
-  document.getElementById('name-of-mission').textContent = spaceXFlights[0].name
-  document.getElementById('description-of-mission').textContent = spaceXFlights[0].description
-  document.getElementById('countdown-clock').textContent = spaceXFlights[0].time
-  document.getElementById('location-of-launch').textContent = spaceXFlights[0].location
+
+class Flights {
+  constructor(name, details, time, location, container, nameBox, detailsBox, timeBox, locationBox) {
+    this.name = name
+    this.details = details
+    this.time = time
+    this.location = location
+    this.container = container
+    this.nameBox = nameBox
+    this.detailsBox = detailsBox
+    this.timeBox = timeBox
+    this.locationBox = locationBox
+  }
 }
+
+const spaceFlights = []
+let currentFlight = 0
+
+const getSpaceXMissions = async () => {
+  const response = await fetch(`https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming`)
+  const missionData = await response.json()
+
+  missionData.forEach(mission => {
+    const cardContainer = document.createElement('div')
+    const cardTitle = document.createElement('h2')
+    const cardDetails = document.createElement('p')
+    const cardTime = document.createElement('p')
+    const cardLocation = document.createElement('p')
+
+    // This is setting up each mission name //
+    const missionName = mission.mission_name
+
+    // This will cover the mission details //
+    const missionDetails = mission.detailsBox
+
+    // This will cover the mission timer //
+    const missionTimer = mission.launch_date_utc
+
+    // This will cover the mission location //
+    const missionLocation = mission.launch_site.site_name_long
+
+    const flights = new Flights(missionName, missionDetails, missionTimer, missionLocation, cardContainer, cardTitle, cardTime, cardDetails, cardLocation)
+    console.log(spaceFlights)
+    spaceFlights.push(flights)
+  })
+  letsDisplayThis()
+}
+
+const letsDisplayThis = () => {
+  console.log(spaceFlights[currentFlight])
+  const mainContainer = document.querySelector('#container')
+
+  mainContainer.textContent = ''
+  spaceFlights[currentFlight].container.classList.add('main-container')
+
+  spaceFlights[currentFlight].nameBox.textContent = spaceFlights[currentFlight].name
+
+  spaceFlights[currentFlight].detailsBox.textContent = spaceFlights[currentFlight].details
+  spaceFlights[currentFlight].detailsBox.classList.add('card-style')
+
+  spaceFlights[currentFlight].timeBox.textContent = spaceFlights[currentFlight].time
+  spaceFlights[currentFlight].timeBox.classList.add('card-style')
+
+  spaceFlights[currentFlight].locationBox.textContent = spaceFlights[currentFlight].location
+  spaceFlights[currentFlight].locationBox.classList.add('card-style')
+
+  spaceFlights[currentFlight].container.appendChild(spaceFlights[currentFlight].nameBox)
+  spaceFlights[currentFlight].container.appendChild(spaceFlights[currentFlight].detailsBox)
+  spaceFlights[currentFlight].container.appendChild(spaceFlights[currentFlight].timeBox)
+  spaceFlights[currentFlight].container.appendChild(spaceFlights[currentFlight].locationBox)
+
+  mainContainer.appendChild(spaceFlights[currentFlight].container)
+}
+
+const rightArrow = () => {
+  if (currentFlight === spaceFlights.length - 1) {
+    currentFlight = 0
+  } else {
+    currentFlight++
+  }
+  letsDisplayThis()
+}
+
+const leftArrow = () => {
+  if (currentFlight === 0) {
+    currentFlight = spaceFlights.length - 1
+  } else {
+    currentFlight--
+  }
+  letsDisplayThis()
+}
+
+document.querySelector('.left-arrow').addEventListener('click', leftArrow)
+document.querySelector('.right-arrow').addEventListener('click', rightArrow)
 document.addEventListener('DOMContentLoaded', main)
